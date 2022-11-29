27976e6ee6f71d8d81f063b37994409e0839b98b
Hemant <hemantsharmavbn36@gmail.com>
2022-11-29 20:17:43 +0530

    trail.js

--- trail.js
+++ trail.js
@@ -1,1 +1,8 @@
-Hi Hemant Sharma this side, how are you?
+let a=[5,7,6,9,8,10];
+let bag="";
+let count=0;
+for(i=1;i<a.length;i++){
+  if(a[i]>a[i-1]&&a[i]>a[i+1]){
+    count++;
+  }
+}console.log(count);
